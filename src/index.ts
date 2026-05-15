d03f0ef5673dd268aea0d79c86302c3b1b8b62c5
Sharkord Developer <dev@sharkord.local>
2026-05-15 16:16:23 +0200

    feat: wire commands into plugin entry point

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,29 @@
 import { PluginConfig, PluginContext } from "@sharkord/plugin-sdk";
+import { registerStreamStartCommand } from "./commands/stream-start";
+import { registerStreamStopCommand } from "./commands/stream-stop";
+import { registerStreamInfoCommand } from "./commands/stream-info";
+import { clearAllTokens } from "./services/token-service";
+import { clearAllStreams } from "./services/stream-manager";
+import { PLUGIN_NAME, VERSION, RTMP_PORT } from "./utils/constants";
 
 export default function plugin(context: PluginContext): PluginConfig {
-  context.log(`🔌 sharkord-stream-with-friends loaded`);
+  const serverIp = "127.0.0.1";
+
+  context.log(`${PLUGIN_NAME} v${VERSION} initializing`);
 
   return {
-    name: "sharkord-stream-with-friends",
-    version: "0.1.0-alpha.1",
+    name: PLUGIN_NAME,
+    version: VERSION,
     onLoad() {
-      context.log("Plugin loaded");
+      registerStreamStartCommand(context, serverIp);
+      registerStreamStopCommand(context);
+      registerStreamInfoCommand(context);
+      context.log(`${PLUGIN_NAME} commands registered`);
     },
     onUnload() {
-      context.log("Plugin unloaded");
+      clearAllTokens();
+      clearAllStreams();
+      context.log(`${PLUGIN_NAME} cleaned up`);
     },
   };
 }
